64d922d4faf1919293a1a0b32bc540343b69a292
Devendra4772 <devendrakumaryadav532@gmail.com>
2025-09-23 12:20:04 +0530

    23 September 2025

--- src/Unit2/Thread2.java
+++ src/Unit2/Thread2.java
@@ -1,12 +1,13 @@
 package Unit2;
 
-public class Thread1 extends Thread {
+public class Thread2 extends Thread {
     public void run() {
-        for (int i = 0; i < 10; i++) {
+        for(int i=0;i<10;i++){
 //            System.out.println(i);
-            try {
-                Thread.sleep(10000);
-            } catch (Exception e) {
+            try{
+                Thread.sleep(1000);
+            }
+            catch(Exception e) {
                 System.out.println(e.getMessage());
             }
             System.out.println(i);
